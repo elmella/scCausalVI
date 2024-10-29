3bf7774376a3cfd8cfb307ee56ba395680969725
sa3520 <shaokunan1@gmail.com>
2024-10-29 11:05:16 -0400

    add theme package

--- docs/conf.py
+++ docs/conf.py
@@ -7,10 +7,8 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import sphinx_rtd_theme
+import sphinx_autodoc_typehints
 
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_static_path = ['_static']
 
 project = 'scCausalVI'
 copyright = '2024, ShaokunAn'
@@ -31,6 +29,31 @@ source_suffix = {
     ".rst": "restructuredtext",
     ".md": "markdown",
 }
+
+autosummary_generate = True
+autodoc_member_order = 'bysource'
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_use_rtype = True  # having a separate entry generally helps readability
+napoleon_use_param = True
+napoleon_custom_sections = [('Params', 'Parameters')]
+todo_include_todos = False
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_logo = '_static/uniPort.png'
+
+
 
 
 language = 'Markdown'
