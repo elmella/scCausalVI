c797aaf1cb730e3509e6cb4e32517d7df6aff1da
sa3520 <shaokunan1@gmail.com>
2024-10-28 21:24:36 -0400

    update docs files

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sphinx_rtd_theme
+
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_static_path = ['_static']
+
 project = 'scCausalVI'
 copyright = '2024, ShaokunAn'
 author = 'ShaokunAn'
@@ -14,15 +20,24 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'myst_parser']
+extensions = ['sphinx.ext.autodoc', 'sphinxcontrib.napoleon', 'myst_parser',
+              'sphinx.ext.napoleon',
+              'sphinx_autodoc_typehints']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+
 language = 'Markdown'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+autodoc_typehints = "description"
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+
+
